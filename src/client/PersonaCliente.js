9d1c8f0be14ac6c126c191acc0880efcfb1d6d86
YaniryFlorez <ymflorezrivera@gmail.com>
2025-02-19 05:47:38 +0000

    tarea 14

--- src/client/PersonaCliente.js
+++ src/client/PersonaCliente.js
@@ -1,39 +1,48 @@
-import axios from "axios";
 
-const obtenerPorId = async(id)=>{
-    //http://localhost:8080/matriculaAPI/v1.1/personas/1
-    axios.get(`http:localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r=>r.data);
-     console.log(data);
-     return data;
+import axios from 'axios';
+
+const obtenerPorId = async (id) => {
+    const response = await axios.get(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`);
+    console.log(response.data);
+    return response.data;
+
 }
 
-const insertar = async(body)=>{
-    axios.post('http:localhost:8081/matriculaAPI/v1.1/personas',body).then(r=>r.data);
-    
+const insertar = async (body) => {
+    axios.post(`http://localhost:8081/matriculaAPI/v1.1/personas`, body).then(r => r.data);
+
 }
 
-const actualizarParcial = async(id,body)=>{
-    axios.pach(`http:localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r=>r.data);
-    
+const actualizar = async (id, body) => {
+    axios.put(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, body).then(r => r.data);
 }
 
-const actualizar = async(id,body)=>{
-    axios.put(`http:localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r=>r.data);
-    
+const actualizarParcial = async (id, body) => {
+    axios.patch(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`, body).then(r => r.data);
 }
 
-const eliminar = async(id)=>{
-    axios.delete(`http:localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r=>r.data);
-    
+const brrar = async(id) =>{
+    axios.delete(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r => r.data);    
 }
 
 
 
-export const obtenerPorFachada = async(id)=>{
+export const obtenerPorIdFachada  = async (id) => {
     return await obtenerPorId(id);
+}
 
+export const insertarFachada = async (body) => {
+    await insertar(body);
 }
-const insertarFachada = async(body)=>{
-   await insertar(body);
-    
+
+export const actualizarFachada  = async (id, body) => {
+    return await actualizar(id, body);
 }
+
+export const actualziarParcialFachada = async (id, body) => {
+    await actualizarParcial(id, body);
+}
+export const eliminarachada  = async (id) => {
+    return await brrar(id);
+}
+
--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -1,124 +1,167 @@
 <template>
-    <div class="form-wrapper">
-        <p><label class="label-text" for="id">id:</label></p>
-        <input id='id' type="text">
+  <div class="form-wrapper">
+    <p><label class="label-text" for="id">id:</label></p>
+    <input id="id" type="text" v-model="id" />
 
-        <p><label class="label-text" for="idnombre">Nombre:</label></p>
-        <input id='idnombre' type="text">
-       
-        <p><label class="label-text" for="idapellido">Apellido:</label></p>
-        <input id="idapellido" type="text">
-       
-        <p><label class="label-text" for="idfecha">Fecha de Nacimiento:</label></p>
-        <input id="idfecha" type="text">
-       
-        <div class="button-group">
-            <button class="save-btn">Guardar</button>
-            <button class="cancel-btn">Cancelar</button>
-            <button v-on:click="buscar()" class="cancel-btn">Consultar</button>
+    <p><label class="label-text" for="idnombre">Nombre:</label></p>
+    <input id="idnombre" type="text" v-model="nombre" />
 
-        </div>
+    <p><label class="label-text" for="idapellido">Apellido:</label></p>
+    <input id="idapellido" type="text" v-model="apellido" />
+
+    <p><label class="label-text" for="idfecha">Fecha de Nacimiento:</label></p>
+    <input id="idfecha" type="text" v-model="fechaNacimiento" />
+
+    <div class="button-group">
+      <button class="cancel-btn" v-on:click="guardar()">Guardar</button>
+      <button class="save-btn" v-on:click="eliminar()">Eliminar</button>
+      <button class="cancel-btn" v-on:click="buscar()">Consultar</button>
+      <button class="cancel-btn" v-on:click="actualizar()">
+        Actualizar Total
+      </button>
+      <button class="cancel-btn" v-on:click="actualziarParcial()">
+        Actualizar Parcial
+      </button>
     </div>
+  </div>
 </template>
  
 <script>
-import { obtenerPorIdFachada } from "../client/PersonaCliente";
+import {
+  obtenerPorIdFachada,
+  insertarFachada,
+  actualizarFachada,
+  actualziarParcialFachada,
+  eliminarachada,
+} from "../client/PersonaCliente";
 export default {
-    mounted(){
-        obtenerPorIdFachada(3);
-        console.log("si existe");
-    },
-    methods:{
-       async buscar(){
-            const data = await obtenerPorIdFachada(this.id);
-            data.nombre= data.nombre;
-        },
-       async guardar(){
-            const bodyPersona={
-                nombre: this.nombre,
-                apellido: this.apellido,
-                fechaNacimiento:this.fechaNacimiento,
+  data() {
+    return {
+      id: "",
+      nombre: "",
+      apellido: "",
+      fechaNacimiento: "",
+    };
+  },
 
-            }
-            await insertarFachada(bodyPersona);
-        }
-    }
+  methods: {
+    async buscar() {
+      const data = await obtenerPorIdFachada(this.id);
+      this.nombre = data.nombre;
+      this.apellido = data.apellido;
+      this.fechaNacimiento = data.fechaNacimiento;
+    },
+    async guardar() {
+      const bodyPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        fechaNacimiento: this.fechaNacimiento,
+      };
+      await insertarFachada(bodyPersona);
+      console.log("se guardo");
+    },
+    async actualizar() {
+      const bodyPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        fechaNacimiento: this.fechaNacimiento,
+      };
+      await actualizarFachada(this.id, bodyPersona);
+      console.log("se actualizarFachada");
+    },
+    async actualziarParcial() {
+      const bodyPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        fechaNacimiento: this.fechaNacimiento,
+      };
+      await actualziarParcialFachada(this.id, bodyPersona);
+      console.log("se actualziarParcialFachada");
+    },
+
+    async eliminar() {
+      await eliminarachada(this.id);
+      console.log("se eliminarachad");
+    },
+  },
 };
 </script>
  
 <style>
-    .form-wrapper {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-start;
-        padding: 20px;
-        border-radius: 12px;
-        background-color: #fde2e4;
-        width: 20%;
-        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-        margin: 30px auto;
-    }
- 
-    .label-text {
-        font-size: 14px;
-        color: #a52a6d;
-        font-weight: 600;
-    }
- 
-    p {
-        margin-bottom: 5px;
-        font-weight: bold;
-        font-size: 16px;
-        color: #7d387d;
-    }
-   
-    input {
-        width: 100%;
-        border: none;
-        border-bottom: 2px solid #dda0dd;
-        background-color: #fff5f8;
-        padding: 8px;
-        border-radius: 5px;
-        font-size: 14px;
-    }
- 
-    input:focus {
-        outline: none;
-        border-bottom: 2px solid #a52a6d;
-    }
- 
-    .button-group {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        margin-top: 20px;
-    }
- 
-    .save-btn, .cancel-btn {
-        padding: 10px 20px;
-        margin: 5px;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 14px;
-        border: none;
-    }
- 
-    .save-btn {
-        background-color: #ff69b4;
-        color: white;
-    }
- 
-    .save-btn:hover {
-        background-color: #d1478d;
-    }
- 
-    .cancel-btn {
-        background-color: #dda0dd;
-        color: white;
-    }
- 
-    .cancel-btn:hover {
-        background-color: #b56ba3;
-    }
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 80px;
+  border-radius: 12px;
+  background-color: #fde2e4;
+  width: 20%;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  margin: 30px auto;
+}
+
+.label-text {
+  font-size: 14px;
+  color: #a52a6d;
+  font-weight: 600;
+}
+
+p {
+  margin-bottom: 5px;
+  font-weight: bold;
+  font-size: 16px;
+  color: #7d387d;
+}
+
+input {
+  width: 100%;
+  border: none;
+  border-bottom: 2px solid #dda0dd;
+  background-color: #fff5f8;
+  padding: 8px;
+  border-radius: 5px;
+  font-size: 14px;
+}
+
+input:focus {
+  outline: none;
+  border-bottom: 2px solid #a52a6d;
+}
+
+.button-group {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 60px;
+
+}
+
+.save-btn,
+.cancel-btn {
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  border: none;
+}
+
+.save-btn {
+  background-color: #ff69b4;
+  color: white;
+}
+
+.save-btn:hover {
+  background-color: #d1478d;
+}
+
+.cancel-btn {
+  background-color: #dda0dd;
+  color: white;
+}
+
+.cancel-btn:hover {
+  background-color: #b56ba3;
+}
 </style>
